d42cebb999897fec39feaefab7847a4c3b62b226
Ivan Grigorev <iv.st.grigorev@gmail.com>
2023-08-07 22:38:34 +0200

    Modified data_visualisation() function.

--- movielens/movielens_handler.py
+++ movielens/movielens_handler.py
@@ -5,8 +5,8 @@ import seaborn as sns
 
 
 # Set pandas display options for DataFrame output
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
+pd.set_option("display.width", None)
+pd.set_option("display.max_colwidth", None)
 
 
 class MovieLens:
@@ -14,28 +14,28 @@ class MovieLens:
         # Create DataFrame from the users table
         self.users = pd.read_table(
             users_file,
-            sep='::',
+            sep="::",
             header=None,
-            names=['user_id', 'gender', 'age', 'occupation', 'zip'],
-            engine='python'
+            names=["user_id", "gender", "age", "occupation", "zip"],
+            engine="python",
         )
 
         # Create DataFrame from the movies table
         self.movies = pd.read_table(
             movies_file,
-            sep='::',
+            sep="::",
             header=None,
-            names=['movie_id', 'title', 'genres'],
-            engine='python'
+            names=["movie_id", "title", "genres"],
+            engine="python",
         )
 
         # Create DataFrame from the ratings table
         self.ratings = pd.read_table(
             ratings_file,
-            sep='::',
+            sep="::",
             header=None,
-            names=['user_id', 'movie_id', 'rating', 'timestamp'],
-            engine='python'
+            names=["user_id", "movie_id", "rating", "timestamp"],
+            engine="python",
         )
 
         # Merge the three DataFrames into one consolidated DataFrame
@@ -43,22 +43,22 @@ class MovieLens:
 
     def movies_rating_count(self):
         # Extract the release year from the title and add a new 'release_year' column as integer type
-        self.data['release_year'] = self.data['title'].str.extract(r'\((\d{4})\)').astype(int)
+        self.data["release_year"] = (
+            self.data["title"].str.extract(r"\((\d{4})\)").astype(int)
+        )
 
         # Filter movies by release year from 1990 to 2000
-        self.data = self.data.loc[(self.data['release_year'] >= 1990) &
-                                  (self.data['release_year'] <= 2000)]
+        self.data = self.data.loc[
+            (self.data["release_year"] >= 1990) & (self.data["release_year"] <= 2000)
+        ]
 
         # Get average movies rating
         mean_rating = self.data.pivot_table(
-            'rating',
-            index='title',
-            columns='gender',
-            aggfunc='mean'
+            "rating", index="title", columns="gender", aggfunc="mean"
         ).dropna()
 
         # Count the number of ratings for each movie
-        rating_by_title = self.data.groupby('title').size()
+        rating_by_title = self.data.groupby("title").size()
 
         # Select movies with high ratings (at least 500 ratings)
         highly_rated_movies = mean_rating.loc[
@@ -78,34 +78,44 @@ class MovieLens:
         mvc = self.movies_rating_count()
 
         mvc = mvc.stack()
-        mvc.name = 'rating'
+        mvc.name = "rating"
         mvc = mvc.reset_index()
 
         # Plotting the first bar plot on the first subplot (ax1)
-        sns.barplot(y='title', x='rating', hue='gender', data=mvc, ax=ax1, palette={'F': 'pink', 'M': 'blue'})
-        ax1.set_title('Average Ratings by Gender for Movies Released between 1990 - 2000')
+        sns.barplot(
+            y="title",
+            x="rating",
+            hue="gender",
+            data=mvc,
+            ax=ax1,
+            palette={"F": "pink", "M": "blue"},
+        )
+        ax1.set_title(
+            "Average Ratings by Gender for Movies Released between 1990 - 2000"
+        )
 
         # Set x-axis limits, ticks, and tick labels
         ax1.set_xlim(0, 5)
         ax1.set_xticks(range(6))
         ax1.set_xticklabels(range(6))
 
-        plt.suptitle('Data from MovieLens 1M', fontsize=18, fontweight='bold')
+        plt.suptitle("Data from MovieLens 1M", fontsize=18, fontweight="bold")
         plt.tight_layout()
         plt.show()
 
     def __repr__(self):
-        return self.data.head(10)
+        return self.data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         movielens = MovieLens(
-            users_file='/Users/a1/PythonProjects/Python_for_Data_Analysis/datasets/movielens/users.dat',
-            movies_file='/Users/a1/PythonProjects/Python_for_Data_Analysis/datasets/movielens/movies.dat',
-            ratings_file='/Users/a1/PythonProjects/Python_for_Data_Analysis/datasets/movielens/ratings.dat'
+            users_file="/Users/a1/PythonProjects/Python_for_Data_Analysis/datasets/movielens/users.dat",
+            movies_file="/Users/a1/PythonProjects/Python_for_Data_Analysis/datasets/movielens/movies.dat",
+            ratings_file="/Users/a1/PythonProjects/Python_for_Data_Analysis/datasets/movielens/ratings.dat",
         )
         # print(movielens.__repr__())
         movielens.movies_rating_count()
+        movielens.data_visualisation()
     except FileNotFoundError as err:
         print(f"{err.strerror}: {err.filename}")
